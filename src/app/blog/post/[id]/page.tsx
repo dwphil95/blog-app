5b75e496d2077ab8924538603755f859c250cb2a
Philip Nguyen <dwphil95@yahoo.com>
2025-01-03 14:05:41 -0800

    Added more npm installs + fixed awaiting dynamic routes

--- src/app/blog/post/[id]/page.tsx
+++ src/app/blog/post/[id]/page.tsx
@@ -1,12 +1,17 @@
 import { posts } from "@/app/lib/placeholder-data";
 import Post from "@/app/ui/components/posts/Post";
 
-export default function Page({params}: {params: {id: string}}) {
-    const post = posts.find((post) => post.id === params.id)
+export default async function Page({
+    params,
+}: {
+    params: Promise<{ id: string }>;
+}) {
+    const { id } = await params;
+    const post = posts.find((post) => post.id === id);
     return (
         <>
             <h1>Post</h1>
-            <Post {...post}/>
+            <Post {...post} />
         </>
     );
 }
